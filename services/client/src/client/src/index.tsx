a097dc019144662c7162e27e0277490bcf6dccce
Miranda Hammarstedt <mh223tt@student.lnu.se>
2018-03-29 17:17:52 +0200

    Adding index.html file and stylesheets, updating Readme.

--- services/client/src/client/src/index.tsx
+++ services/client/src/client/src/index.tsx
@@ -2,8 +2,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 import { App } from './App';
+const styles = require('./assets/sass/main.scss');
 
 ReactDOM.render(
     <App />,
-    document.getElementById('root')
+    document.getElementById('app')
 );
--- services/client/webpack.config.tsx
+++ services/client/webpack.config.tsx
@@ -5,54 +5,54 @@ const DIST_DIR   = path.join(__dirname, 'src/client/public/dist');
 const CLIENT_DIR = path.join(__dirname, 'src/client/src');
 
 module.exports = {
-  context: CLIENT_DIR,
-  entry: './index.tsx',
-  output: {
-    path: DIST_DIR,
-    filename: 'bundle.js'
-  },
-  resolve: {
-    extensions: ['.ts', '.tsx', '.js', '.json', '.scss']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        loader: 'awesome-typescript-loader'
-      },
-      {
-        test: /(\.css|.scss)$/,
-        exclude: /node_modules/,
-        include: path.join(__dirname, 'src/client/src/assets'),
-        use: [ 'style-loader', 'css-loader', 'sass-loader' ]
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: [ 'react', 'es2015' ]
-            }
-          },
-          {
-            loader: 'react-hot-loader/webpack'
-          }
-        ]
-      }
-    ]
-  },
-  devServer: {
-    contentBase: CLIENT_DIR,
-    publicPath: '/',
-    port: 3000,
-    public: 'localhost:3000',
-    overlay: true,
-    host: '0.0.0.0',
-    watchOptions: {
-      poll: false
-    },
-    historyApiFallback: true
-  }
+	context: CLIENT_DIR,
+	entry: './index.tsx',
+	output: {
+		path: DIST_DIR,
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js', '.json', '.scss']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader'
+			},
+			{
+				test: /(\.css|.scss)$/,
+				exclude: /node_modules/,
+				include: path.join(__dirname, 'src/client/src/assets'),
+				use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+			},
+			{
+				test: /\.js$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'babel-loader',
+						options: {
+							presets: [ 'react', 'es2015' ]
+						}
+					},
+					{
+						loader: 'react-hot-loader/webpack'
+					}
+				]
+			}
+		]
+	},
+	devServer: {
+		contentBase: CLIENT_DIR,
+		publicPath: '/',
+		port: 3000,
+		public: 'localhost:3000',
+		overlay: true,
+		host: '0.0.0.0',
+		watchOptions: {
+			poll: false
+		},
+		historyApiFallback: true
+	}
 };
