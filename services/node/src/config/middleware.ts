6b8273d34d89c819dbf35313ed522c8cbd766b2c
Molly Arhammar <mollyarhammar@gmail.com>
2018-04-04 14:07:28 +0200

    refactor

--- services/node/src/config/middleware.ts
+++ services/node/src/config/middleware.ts
@@ -8,13 +8,16 @@
  import IMAPHandler from './../handlers/email/IMAPHandler';
 
  class Middleware {
-   static accessToken: string;
-   static refreshToken: string;
+   static IMAPaccessToken: string;
+   static IMAPrefreshToken: string;
 
+   /**
+    * Checks if there are access and refresh tokens or if they have changed, and tn that case updates the IMAP connection.
+    */
    public updateIMAPConnection (req: Request, res: Response, next: NextFunction): void {
-    if ((process.env.IMAP_ACCESS_TOKEN && process.env.IMAP_REFRESH_TOKEN) && (process.env.IMAP_ACCESS_TOKEN !== Middleware.accessToken || process.env.IMAP_REFRESH_TOKEN !== Middleware.refreshToken)) {
-      Middleware.accessToken = process.env.IMAP_ACCESS_TOKEN;
-      Middleware.refreshToken = process.env.IMAP_REFRESH_TOKEN;
+    if ((process.env.IMAP_ACCESS_TOKEN && process.env.IMAP_REFRESH_TOKEN) && (process.env.IMAP_ACCESS_TOKEN !== Middleware.IMAPaccessToken || process.env.IMAP_REFRESH_TOKEN !== Middleware.IMAPrefreshToken)) {
+      Middleware.IMAPaccessToken = process.env.IMAP_ACCESS_TOKEN;
+      Middleware.IMAPrefreshToken = process.env.IMAP_REFRESH_TOKEN;
       IMAPHandler.connect(IMAPConnection);
     }
       return next();
