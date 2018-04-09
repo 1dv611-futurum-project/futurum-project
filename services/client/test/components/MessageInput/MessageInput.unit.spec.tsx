e36d450c609f764ca9905bc1c877e3fd99ce96aa
Jesper <[jp222vq@student.lnu.se]>
2018-04-10 00:26:40 +0200

    added tests for MessageInput component

---
+++ services/client/test/components/MessageInput/MessageInput.unit.spec.tsx
@@ -0,0 +1,49 @@
+import * as mocha from 'mocha';
+import * as React from 'react';
+import { mount, shallow } from 'enzyme';
+import { expect } from 'chai';
+
+import TextArea from 'react-textarea-autosize';
+import { Button } from 'material-ui';
+import { MessageInput } from '../../../src/components/MessageInput/MessageInput';
+
+describe('<MessageInput />', () => {
+	let wrapper: any;
+	const props = {
+		onClick: (message: string) => {},
+		open: true
+	};
+
+	before(() => {
+		wrapper = shallow(<MessageInput {...props}/>);
+	});
+
+	it('should show message input if open', () => {
+		expect(wrapper.find('.message-input--hidden')).to.have.length(0);
+	});
+
+	it('should hide message input if not open', () => {
+		props.open = false;
+		wrapper = shallow(<MessageInput {...props}/>);
+
+		expect(wrapper.find('.message-input--hidden')).to.have.length(1);
+	});
+
+	it('should update state on input', () => {
+		wrapper.find(TextArea).simulate('change', {target: {value: 'test'}});
+		expect(wrapper.state('message')).to.equal('test');
+	});
+
+	it('should send the input message', () => {
+		let expected;
+		let msg = 'test';
+
+		props.onClick = (message: string) => { expected = message };
+		wrapper = shallow(<MessageInput {...props}/>);
+		wrapper.setState({ message: msg });
+
+		wrapper.find(Button).simulate('click');
+		expect(msg).to.equal(expected);
+	});
+
+});
--- services/client/test/index.test.tsx
+++ services/client/test/index.test.tsx
@@ -4,5 +4,6 @@ import './components/AddButton/AddButton.unit.spec';
 import './components/ClientList/ClientList.unit.spec';
 import './components/Header/Header.unit.spec';
 import './components/Message/Message.unit.spec';
+import './components/MessageInput/MessageInput.unit.spec'
 import './components/StatusSelect/StatusSelect.unit.spec';
 import './components/Ticket/Ticket.unit.spec';
