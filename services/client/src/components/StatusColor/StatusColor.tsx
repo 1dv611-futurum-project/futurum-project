16c800a86c3c55ed7437c6f9658420adb3b4c460
Jesper <[jp222vq@student.lnu.se]>
2018-04-10 14:16:32 +0200

    Added proof of concept settings page

---
+++ services/client/src/components/StatusColor/StatusColor.tsx
@@ -0,0 +1,80 @@
+/**
+ * StatusSelect component
+ * @module components/StatusSelect/StatusSelect
+ */
+
+import * as React from 'react';
+import { Button } from 'material-ui';
+import { SketchPicker } from 'react-color';
+
+/**
+ * StatusColor Props Interface
+ */
+export interface IStatusColor {
+    color: string;
+    status: string;
+    onChange(color: any): void;
+}
+
+/**
+ * Proof Of Concept
+ * StatusColor class
+ */
+export class StatusColor extends React.Component<IStatusColor, any> {
+
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            displayColorPicker: false,
+            color: this.props.color
+        };
+    }
+
+    public render() {
+        const { color, status } = this.props;
+
+        return (
+            <div className='status-color__select' onClick={this.handleClick}>
+                {this.state.displayColorPicker ?
+                    <SketchPicker color={this.state.color} onChange={this.handleChange} />
+                    :
+                    <div>
+                        <Button variant="raised" className={`status-color status-color--${color}`} onClick={this.handleClose}>
+                        &nbsp;</Button>
+                        <span className='status-color__desc'>{status}</span>
+                    </div>
+                }
+            </div>
+        );
+    }
+
+    private getColor(color: string) {
+        switch(color) {
+            case 'red': {
+                this.setState({color: '#f1453d'});
+                break;
+            }
+            case 'blue': {
+                this.setState({color: '#1fbcd2'});
+                break;
+            }
+            case 'green': {
+                this.setState({color: '#50ae55'});
+                break;
+            }
+        }
+    }
+
+    private handleClick = () => {
+        this.setState({ displayColorPicker: !this.state.displayColorPicker })
+    };
+
+    private handleClose = () => {
+        this.setState({ displayColorPicker: false })
+    };
+
+    private handleChange = (color: any) => {
+        this.setState({ color: color })
+        this.props.onChange(color);
+    };
+}
--- services/client/src/containers/SettingsPage/SettingsPage.tsx
+++ services/client/src/containers/SettingsPage/SettingsPage.tsx
@@ -4,8 +4,12 @@
  */
 
 import * as React from 'react';
+import { Divider } from 'material-ui';
+import { StatusColor } from '../../components/StatusColor/StatusColor';
+import { AddButton } from '../../components/AddButton/AddButton';
 
 /**
+ * Proof Of Concept
  * SettingsPage class
  */
 export class SettingsPage extends React.Component<any, any> {
@@ -13,8 +17,20 @@ export class SettingsPage extends React.Component<any, any> {
 	public render() {
 		return (
 			<div className='settings__wrapper'>
-				Settings page.
+				<h1 className='client-list-page__header__title'>Redigera statusfärger</h1>
+				<StatusColor color={'red'} status={'Ej påbörjad'} onChange={this.onChange} />
+				<StatusColor color={'blue'} status={'Påbörjad'} onChange={this.onChange} />
+				<StatusColor color={'green'} status={'Genomförd'} onChange={this.onChange} />
+				<StatusColor color={'green'} status={'Stängd'} onChange={this.onChange} />
+				<br />
+				<Divider />
+				<h1 className='client-list-page__header__title'>Redigera ansvariga</h1>
+				<AddButton text='Lägg till ansvariga' onClick={() => {}}/>
 			</div>
 		);
+	}
+
+	private onChange(color: string) {
+		console.log(color);
 	}
 }
