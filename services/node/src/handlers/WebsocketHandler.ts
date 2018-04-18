3d58cc5a96da26557ecc5324c6e76e473a79e2e4
Molly Arhammar <mollyarhammar@gmail.com>
2018-04-18 10:19:35 +0200

    finish merge

--- services/node/src/handlers/WebsocketHandler.ts
+++ services/node/src/handlers/WebsocketHandler.ts
@@ -10,7 +10,6 @@ const io = require('socket.io')({ path: '/socket' });
  */
 class WebsocketHandler {
 
-<<<<<<< HEAD
   private io: Socket;
   private port = 3001;
 
@@ -50,53 +49,6 @@ class WebsocketHandler {
       this.io.emit('socket', JSON.stringify(data));
     } catch (error) {
       console.error(error);
-=======
-    private io: Socket;
-    private port = 3001;
-
-    constructor() {
-        this.io = io;
-    }
-
-    /**
-     * Starts the socket connection
-     */
-    public listen(port, callback): void {
-        this.port = port || this.port;
-
-        this.io.listen(port);
-        this.io.on('connection', (socket: any) => {
-            console.log('Connected client on port %s.', this.port);
-            io.to(socket.id).emit('socket', { id: socket.id });
-
-            socket.on('message', (m: any) => {
-                console.log('[server](message): %s', JSON.stringify(m));
-                this.io.emit('message', m);
-            });
-
-            socket.on('disconnect', () => {
-                console.log('Client disconnected');
-            });
-        });
-        callback();
-    }
-
-    /**
-     * Emits data to the client on all channels.
-     */
-    public emit(data: Array<Object>): void{
-        try {
-            // Logic to see if socket is connected?
-            this.io.emit('socket', JSON.stringify(data));
-        } catch (error) {
-            console.error(error);
-        }
-    }
-
-    private originIsAllowed(origin): boolean {
-        // put logic here to detect whether the specified origin is allowed.
-        return true;
->>>>>>> client-ws
     }
   }
 
