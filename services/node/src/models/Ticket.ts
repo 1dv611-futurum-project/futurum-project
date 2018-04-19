39165f57d04c0a39f05dbbbb219b04b965e36206
Johan Söderlund <js223zs@student.lnu.se>
2018-04-19 11:47:35 +0200

    Message model

--- services/node/src/models/Ticket.ts
+++ services/node/src/models/Ticket.ts
@@ -4,16 +4,11 @@
 
 import { Document, Schema, Model, model} from 'mongoose';
 import { ITicket } from './interfaces/ITicket';
+import Message from './Message';
 
 export interface ITicketModel extends ITicket, Document {}
 
  // Todo: change Date to DateTime type
-let Message: object = {
-  recieved: {type: Date, required: true},
-  fromCustomer: {type: Boolean, required: true},
-  body: {type: String, required: true}
-};
-
 const TicketSchema: Schema = new Schema({
   recieved: {type: Date, required: true},
   title: {type: String, required: true},
