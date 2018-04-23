5f807de17684efb591f8d68faf780c828222afd7
Molly Arhammar <mollyarhammar@gmail.com>
2018-04-23 10:32:44 +0200

    tests for imap-connection

--- services/node/test/email-tests/imap-connection-test-suite.ts
+++ services/node/test/email-tests/imap-connection-test-suite.ts
@@ -4,12 +4,10 @@
 
 // Requires
 let expect = require('chai').expect;
-import IMAPConnectionInterface from './../../src/handlers/email/IMAPConnectionInterface';
 import IMAPConnection from './../../src/handlers/email/IMAPConnection';
 
 // Tests
 describe('IMAPConnection', () => {
-
   it('should implement the IMAPConnectionInterface', (done) => {
     expect(typeof IMAPConnection.updateCredentials).to.equal('function');
     expect(typeof IMAPConnection.getUnreadEmails).to.equal('function');
