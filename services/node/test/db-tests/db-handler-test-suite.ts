a47c29cc42ccf12242ab2d3d40eb0b312731b82c
Molly Arhammar <mollyarhammar@gmail.com>
2018-04-23 10:04:32 +0200

    tests for xoauth

--- services/node/test/db-tests/db-handler-test-suite.ts
+++ services/node/test/db-tests/db-handler-test-suite.ts
@@ -3,55 +3,33 @@
  */
 
 // Requires
-let expect = require('chai').expect
-let sinon = require('sinon')
-let mongoose = require('mongoose')
-let correctConnectionString = 'mongodb://futurum-db:27017'
+let expect = require('chai').expect;
+let sinon = require('sinon');
+let mongoose = require('mongoose');
+let correctConnectionString = 'mongodb://futurum-db:27017';
 
-import DBHandler from './../../src/handlers/db/DBHandler'
-import DBConnection from './../../src/handlers/db/DBConnection'
-let DBConnectionInstance = new DBConnection()
-let sut = new DBHandler(DBConnectionInstance)
-sut.on('error', () => {})
+import DBHandler from './../../src/handlers/db/DBHandler';
+import DBConnection from './../../src/handlers/db/DBConnection';
+let DBConnectionInstance = new DBConnection();
+let sut = new DBHandler(DBConnectionInstance);
+sut.on('error', () => {});
 
 // Tests
 describe('DBHandler', () => {
   after(() => {
     return new Promise((resolve) => {
-      mongoose.connection.close(resolve)
-    })
-  })
+      mongoose.connection.close(resolve);
+    });
+  });
 
   describe('connect()', () => {
     it('should connect the database', () => {
       return new Promise((resolve) => {
-        sut.on('ready', resolve)
-        let spy = sinon.spy(DBConnectionInstance, "connect")
-        sut.connect(correctConnectionString)
-        expect(spy.called).to.equal(true)
-      })
-    })
-  })
-
-  describe('addOrUpdate()', () => {
-    let correctCustomerInfo = {name: 'Othman El Kabir', email: 'othman@dif.se'}
-    let incorrectCustomerInfo = {notName: 'Othman El Kabir'}
-    let correctType = 'customer'
-    let incorrectType = 'notcustomer'
-
-    describe('correct add of correct type', () => {
-      it('should add the customer to the database if there  is no such customer already', (done) => {
-          sut.addOrUpdate(correctType, correctCustomerInfo)
-            .then(() => {
-              return sut.getOne(correctType, correctCustomerInfo)
-            })
-            .then((customer) => {
-              expect(customer).to.exist
-              expect(customer.name).to.equal(correctCustomerInfo.name)
-              expect(customer.email).to.equal(correctCustomerInfo.email)
-              done()
-            })
-        })
-      })
-    })
-})
+        sut.on('ready', resolve);
+        const spy = sinon.spy(DBConnectionInstance, 'connect');
+        sut.connect(correctConnectionString);
+        expect(spy.called).to.equal(true);
+      });
+    });
+  });
+});
--- services/node/test/testrunner.test.ts
+++ services/node/test/testrunner.test.ts
@@ -3,17 +3,18 @@
  */
 
 // Variables
-let expect = require('chai').expect
+let expect = require('chai').expect;
 
 describe('testrunner', () => {
   it('should return pass on 1 + 1 equals 2', (done) => {
-    expect(1 + 1).to.equal(2)
-    done()
-  })
-})
+    expect(1 + 1).to.equal(2);
+    done();
+  });
+});
 
 // Suites
-require('./db-tests/mongoose-models-test-suite')
-require('./db-tests/db-connection-test-suite')
-require('./db-tests/db-handler-test-suite')
-require('./email-tests/email-handler-test-suite')
+require('./db-tests/mongoose-models-test-suite');
+require('./db-tests/db-connection-test-suite');
+require('./db-tests/db-handler-test-suite');
+require('./email-tests/email-handler-test-suite');
+require('./email-tests/xoauth-generator-test-suite');
