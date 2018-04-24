517f3a2bbbfd7eb8d03f7a31709acda3eee633e3
Molly Arhammar <mollyarhammar@gmail.com>
2018-04-24 11:28:07 +0200

    clean up interfaces

--- services/client/server/index.tsx
+++ services/client/server/index.tsx
@@ -1,32 +1,14 @@
 import * as http from 'http';
 import App from './App';
 
-
 import * as io from 'socket.io';
-
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(App);
-
-//let ioServer: SocketIO.Server = io(server);
 
 App.listen(port, (err) => {
 	if (err) {
 		return console.log(err);
 	}
-
 	return console.log(`Server is listening on ${port}`);
 });
-
-
-/*
-ioServer.on('connection', function (socket: SocketIO.Socket): void {
-    console.log('Socket connected');
-    socket.on('serverName', function (): void {
-        socket.emit('serverName', {
-            name: 'expressjs'
-        });
-        console.log('Socket received serverName');
-    });
-});
-*/
