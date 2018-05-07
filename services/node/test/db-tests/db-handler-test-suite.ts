ba288121d3a82b1137ce7f274c2ebd45a8f27874
Johan Söderlund <js223zs@student.lnu.se>
2018-05-07 17:52:17 +0200

    db-test

--- services/node/test/db-tests/db-handler-test-suite.ts
+++ services/node/test/db-tests/db-handler-test-suite.ts
@@ -10,6 +10,7 @@ import * as mongoose from 'mongoose';
 
 import DBHandler from './../../src/handlers/db/DBHandler';
 import DBConnection from './../../src/handlers/db/DBConnection';
+import Ticket from './../../src/handlers/db/models/Ticket';
 
 const DBConnectionInstance = new DBConnection();
 const sut = new DBHandler(DBConnectionInstance);
@@ -36,6 +37,57 @@ export function run() {
           const spy = sinon.spy(DBConnectionInstance, 'connect');
           sut.connect(correctConnectionString);
           expect(spy.called).to.equal(true);
+        });
+      });
+    });
+
+    describe('createNewFromType() ticket', () => {
+      it('should store new instance of Ticket in database', () => {
+        return new Promise((resolve) => {
+          const expected = new Ticket({from: 'mock@mail.com', body: []});
+          sut.createNewFromType('ticket', expected)
+            .then((saved) => {
+              expect(JSON.stringify(saved)).to.equal(JSON.stringify(expected));
+            });
+        });
+      });
+    });
+
+    describe('getOne() ticket', () => {
+      it('should return one object of specific type', () => {
+        return new Promise((resolve) => {
+          const expected = new Ticket({from: 'mock@mail.com', body: []});
+          sut.getOne('ticket', expected).then( (ticket) => {
+            expect(JSON.stringify(ticket)).to.equal(JSON.stringify(expected));
+          });
+        });
+      });
+    });
+
+    describe('addOrUpdate() ticket', () => {
+      it('should update ticket instance in the database', () => {
+        return new Promise((resolve) => {
+          const expected = new Ticket({title: 'Updated title', from: 'mock@mail.com', body: []});
+          const oldTicket = new Ticket({from: 'mock@mail.com', body: []});
+          sut.addOrUpdate('ticket', expected).then((ticket) => {
+            sut.getOne('ticket', oldTicket).then((t) => {
+              expect(JSON.stringify(t)).to.equal(JSON.stringify(expected));
+            });
+          });
+        });
+      });
+    });
+
+    describe('removeOne() ticket', () => {
+      it('should remove ticket instance added in sut', () => {
+        return new Promise((resolve) => {
+          const removeOn = new Ticket({title: 'Updated title', from: 'mock@mail.com', body: []});
+          sut.removeOne('ticket', removeOn);
+          sut.getOne('ticket', removeOn).then( (ticket) => {
+            expect(ticket).to.equal(null);
+            // chai.should().equal(ticket, null);
+            // expect(ticket).to.be.null;
+          });
         });
       });
     });
