5ff86d08e2bd4d0882a6fdece4b7e44edca46abf
Miranda Hammarstedt <mh223tt@student.lnu.se>
2018-05-08 17:11:42 +0200

    Adjusting Message component.

--- services/client/src/components/Message/Message.tsx
+++ services/client/src/components/Message/Message.tsx
@@ -27,7 +27,7 @@ export class Message extends React.Component<IMessage, any> {
 	 * @public
 	 */
 	public render() {
-		const { body, fromCustomer, received } = this.props.data;
+		const { body, fromCustomer, received, fromName } = this.props.data;
 		const { customer } = this.props;
 		const assignee = this.props.assignee ? this.props.assignee.name : 'Futurum Digital';
 
@@ -38,7 +38,7 @@ export class Message extends React.Component<IMessage, any> {
 				</Avatar>
 				<div className='message__content'>
 					<p className='message__content__title'>
-						{fromCustomer ? customer : assignee},
+						{fromCustomer ? fromName || customer : fromName || assignee},
 						<span className='message__content__title--regular'>
 							{` ${this.getFormattedDate(received)}`}
 						</span>
